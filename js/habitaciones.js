d5760ef6009c8d890a1e6df09e1655d71e65dc40
unknown <juansebastian.012@gmail.com>
2019-02-24 19:48:23 -0300

    DemoDuoGamingWordpressTemplate

---
+++ js/habitaciones.js
@@ -0,0 +1,65 @@
+/*
+$(document).ready(function () {
+    let nextRoom = document.querySelector(".nextRoom");
+    let roomTitle = document.querySelector("#roomTitle");
+
+    let roomImg1 = document.querySelector("#roomImg1");
+    let roomImg2 = document.querySelector("#roomImg2");
+    let roomImg3 = document.querySelector("#roomImg3");
+    let roomImg4 = document.querySelector("#roomImg4");
+    let roomImg5 = document.querySelector("#roomImg5");
+    let roomslider6 = document.querySelector("#roomslider6");
+    let roomslider7 = document.querySelector("#roomslider7");
+    let dataSlide6 = document.querySelector("#dataSlide6");
+    let dataSlide7 = document.querySelector("#dataSlide7");
+    let smallRoomImg1 = document.querySelector("#smallRoomImg1");
+    let smallRoomImg2 = document.querySelector("#smallRoomImg2");
+    let smallRoomImg3 = document.querySelector("#smallRoomImg3");
+    let address = window.location.host;
+    let protocol = window.location.protocol;
+    let containerNextRoom = document.querySelector("#containerNextRoom");
+    let bronceImg = document.querySelector("#bronceImg");
+
+    console.log("something");
+
+
+    nextRoom.addEventListener("click", function () {
+
+        roomTitle.innerHTML = "Habitación Bronce $45.00";
+        /*
+        How to get the address
+        var test = roomImg1.getAttribute("src");
+        console.log(test);
+    
+
+
+        console.log(address);
+        console.log(protocol);
+        roomImg1.setAttribute("src", `${protocol}//${address}/HotelMarbella/wordpress/wp-content/themes/Beta/img/Habitaciones/Bronce/bronce-1.jpg`);
+        roomImg2.setAttribute("src", `${protocol}//${address}/HotelMarbella/wordpress/wp-content/themes/Beta/img/Habitaciones/Bronce/bronce-2.jpg`);
+        roomImg3.setAttribute("src", `${protocol}//${address}/HotelMarbella/wordpress/wp-content/themes/Beta/img/Habitaciones/Bronce/bronce-3.jpg`);
+        roomImg4.setAttribute("src", `${protocol}//${address}/HotelMarbella/wordpress/wp-content/themes/Beta/img/Habitaciones/Bronce/bronce-4.jpg`);
+        roomImg5.setAttribute("src", `${protocol}//${address}/HotelMarbella/wordpress/wp-content/themes/Beta/img/Habitaciones/Bronce/bronce-5.jpg`);
+        roomslider6.remove();
+        roomslider7.remove();
+        dataSlide6.remove();
+        dataSlide7.remove();
+
+        smallRoomImg1.setAttribute("src", `${protocol}//${address}/HotelMarbella/wordpress/wp-content/themes/Beta/img/Habitaciones/Bronce/bronce-3.jpg`);
+        smallRoomImg2.setAttribute("src", `${protocol}//${address}/HotelMarbella/wordpress/wp-content/themes/Beta/img/Habitaciones/Bronce/bronce-4.jpg`);
+        smallRoomImg3.setAttribute("src", `${protocol}//${address}/HotelMarbella/wordpress/wp-content/themes/Beta/img/Habitaciones/Bronce/bronce-5.jpg`);
+
+        /*
+        bronceImg.remove();
+        let standardImg = new Image();
+        standardImg.src(`${protocol}//${address}/HotelMarbella/wordpress/wp-content/themes/Beta/img/habitacion-2.jpg`);
+        document.containerNextRoom.appendChild(standardImg);
+
+
+    });
+
+
+
+});
+
+*/
